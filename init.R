472fb211c64ef9f7633d7855675497c52618b826
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-01-20 14:05:26 -0500

    init installation + renv.lock

---
+++ init.R
@@ -0,0 +1,23 @@
+renv::init()
+install.packages("devtools")
+install.packages("remotes")
+install.packages("colorspace")
+library(remotes)
+install_github(
+  "sysilviakim/Kmisc",
+  INSTALL_opts = c("--no-multiarch"), dependencies = TRUE
+)
+Kmisc::proj_skeleton()
+
+# Install all libraries
+# CRAN tidyverse
+install.packages("plyr")
+install.packages("tidyverse")
+install.packages("lubridate")
+
+# CRAN non-tidyverse
+install.packages("assertthat")
+install.packages("fst")
+install.packages("styler")
+
+renv::snapshot()
