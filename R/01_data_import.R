0239ca2714aadb182258f4e00d179f30046e73bc
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-04 14:30:13 -0500

    first time wrangling in loop (memory const.)

--- R/01_data_import.R
+++ R/01_data_import.R
@@ -8,25 +8,15 @@ if (!file.exists(here("data/tidy/president_2020.fst"))) {
   categories <- c(
     "president", "senate", "house", "actblue", "rightus", "winred"
   )
-  df_ls <- categories %>%
-    set_names(., .) %>%
-    map(
-      ~ list.files(
-        "data/raw",
-        pattern = .x, 
-        full.names = TRUE
-      ) %>%
-        keep(~ grepl("amount", .x)) %>%
-        map(loadRData) %>%
-        bind_rows() %>%
-        as_tibble() %>%
-        dedup()
-    )
-
-  categories %>%
-    map(
-      ~ write_fst(df_ls[[.x]], here(paste0("data/tidy/", .x, "_2020.fst")))
-    )
+  for (c in categories) {
+    df_raw <- list.files("data/raw", pattern = c, full.names = TRUE) %>%
+      keep(~ grepl("amount", .x)) %>%
+      map(loadRData) %>%
+      bind_rows() %>%
+      as_tibble() %>%
+      dedup()
+    write_fst(df_raw, here(paste0("data/tidy/", c, "_2020.fst")))
+  }
 } else {
   # Allow specifying categories outside this script
   if (length(categories) > 1) {
