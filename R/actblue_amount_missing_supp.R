e8cbfd0abaec1496565035b4f3040aade1009088
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-04 22:40:12 -0500

    save actblue_amount_scraped_supp

--- R/actblue_amount_missing_supp.R
+++ R/actblue_amount_missing_supp.R
@@ -33,32 +33,54 @@ amount_missing <- temp %>%
 
 js_supp <- amount_supp <- vector("list", nrow(amount_missing))
 for (i in seq(nrow(amount_missing))) {
-  tryCatch({
-    amount_supp[[i]] <- fundraising_actblue(amount_missing$url[i])
-  }, error = function(e) {
-    amount_supp[[i]] <- NA
-    message(e)
-  })
+  tryCatch(
+    {
+      amount_supp[[i]] <- fundraising_actblue(amount_missing$url[i])
+    },
+    error = function(e) {
+      amount_supp[[i]] <- NA
+      message(e)
+    }
+  )
   Sys.sleep(5)
-  tryCatch({
-    js_supp[[i]] <- actblue_js(amount_missing$url[i])
-  }, error = function(e) {
-    js_supp[[i]] <- NA
-    message(e)
-  })
+  tryCatch(
+    {
+      js_supp[[i]] <- actblue_js(amount_missing$url[i])
+    },
+    error = function(e) {
+      js_supp[[i]] <- NA
+      message(e)
+    }
+  )
   Sys.sleep(5)
   message(i)
 }
 save(js_supp, file = "data/raw/actblue_js_supp_2020.Rda")
 
+js_supp %>%
+  map(is.null) %>%
+  unlist() %>%
+  which() %>%
+  amount_missing[., ] %>%
+  View()
+# 45 unavailable due to 404 error
+
 amount_supp <- seq(nrow(amount_missing)) %>%
   map_dfr(
-    ~ tibble(
-      fundraiser = amount_missing$fund_title[.x],
-      url = amount_missing$url[.x],
-      date = Sys.Date(),
-      portfolio = amount_supp[[.x]]
-    )
+    ~ {
+      if (!is.null(amount_supp[[.x]])) {
+        tibble(
+          fundraiser = amount_missing$fund_title[.x],
+          url = amount_missing$url[.x],
+          date = Sys.Date(),
+          portfolio = amount_supp[[.x]]
+        )
+      } else {
+        NULL
+      }
+    }
   )
 
-save(amount_supp, file = "actblue_amount_scraped_2020_supp.Rda")
+save(
+  amount_supp, file = here("data/raw/actblue_amount_scraped_2020_supp.Rda")
+)
