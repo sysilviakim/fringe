ddac33a63e733ab9dcb45c7dc782fc275b310d8c
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-18 15:52:08 -0500

    winred candidate of the week

---
+++ R/cand_of_week_winred.R
@@ -0,0 +1,165 @@
+cand_of_the_week <- list(
+  tibble(
+    name = "Steve Daines",
+    state_cd = "MO-SEN",
+    start_date = "04062020",
+    end_date = "04132020"
+  ),
+  tibble(
+    name = "Genevieve Collins",
+    state_cd = "TX-32",
+    start_date = "04132020",
+    end_date = "04202020"
+  ),
+  tibble(
+    name = "John James",
+    state_cd = "MI-SEN",
+    start_date = "04202020",
+    end_date = "04272020"
+  ),
+  tibble(
+    name = "Tom Kean",
+    state_cd = "NJ-07",
+    start_date = "04272020",
+    end_date = "05042020"
+  ),
+  tibble(
+    name = "Beth Van Duyne",
+    state_cd = "TX-24",
+    start_date = "05042020",
+    end_date = "05112020"
+  ),
+  tibble(
+    name = "Cindy Hyde-Smith",
+    state_cd = "MS-SEN",
+    start_date = "05112020",
+    end_date = "05182020"
+  ),
+  tibble(
+    name = "Mike Garcia",
+    state_cd = "CA-25",
+    start_date = "05182020",
+    end_date = "06022020" 
+    # May 25 missing from Instagram/Twitter
+  ),
+  tibble(
+    name = "Sean Parnell",
+    state_cd = "PA-17",
+    start_date = "06022020",
+    end_date = "06082020"
+  ),
+  tibble(
+    name = "Tamika Hamilton",
+    state_cd = "CA-03",
+    start_date = "06082020",
+    end_date = "06182020"
+  ),
+  tibble(
+    name = "Jim Bogart",
+    state_cd = "PA-08",
+    start_date = "06182020",
+    end_date = "06232020"
+  ),
+  tibble(
+    name = "Matt Mowers",
+    state_cd = "NH-01",
+    start_date = "06232020",
+    end_date = "06302020"
+  ),
+  tibble(
+    name = "Corky Mesner",
+    state_cd = "NH-SEN",
+    start_date = "06302020",
+    end_date = "07072020"
+  ),
+  tibble(
+    name = "Ronny Jackson",
+    state_cd = "TX-13",
+    start_date = "07072020",
+    end_date = "07132020"
+  ),
+  tibble(
+    name = "Burgess Owens",
+    state_cd = "UT-04",
+    start_date = "07132020",
+    end_date = "07202020"
+  ),
+  tibble(
+    name = "Tommy Tuberville",
+    state_cd = "AL-SEN",
+    start_date = "07202020",
+    end_date = "07272020"
+  ),
+  tibble(
+    name = "Ashley Hinson",
+    state_cd = "IA-01",
+    start_date = "07272020",
+    end_date = "08042020"
+  ),
+  tibble(
+    name = "Nancy Mace",
+    state_cd = "SC-01",
+    start_date = "08042020",
+    end_date = "08102020"
+  ),
+  tibble(
+    name = "Madison Cawthorn",
+    state_cd = "NC-11",
+    start_date = "08102020",
+    end_date = "08212020"
+  ),
+  tibble(
+    name = "Victoria Spartz",
+    state_cd = "IN-05",
+    start_date = "08212020",
+    end_date = "08252020"
+  ),
+  tibble(
+    name = "Kimberly Klacik",
+    state_cd = "MD-07",
+    start_date = "08252020",
+    end_date = "09022020"
+  ),
+  tibble(
+    name = "Anna Paulina Luna",
+    state_cd = "Fl-13",
+    start_date = "09022020",
+    end_date = "09082020"
+  ),
+  tibble(
+    name = "Byron Donalds",
+    state_cd = "FL-19",
+    start_date = "09082020",
+    end_date = "09152020"
+  ),
+  tibble(
+    name = "Dan Rodimer",
+    state_cd = "NV-03",
+    start_date = "09152020",
+    end_date = "09222020"
+  ),
+  tibble(
+    name = "Rich McCormick",
+    state_cd = "GA_07",
+    start_date = "09222020",
+    end_date = "09292020"
+  ),
+  tibble(
+    name = "Lee Zedlin",
+    state_cd = "NY-01",
+    start_date = "09292020",
+    end_date = "10062020"
+  ),
+  tibble(
+    name = "Jeff Van Drew",
+    state_cd = "NJ-02",
+    start_date = "10062020",
+    end_date = "10222020"
+  ),
+  tibble(
+    name = "David Schweikert",
+    state_cd = "AZ-06",
+    start_date = "10222020",
+    end_date = "11022020"
+  )
+)
