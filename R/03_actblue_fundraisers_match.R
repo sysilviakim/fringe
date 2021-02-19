3ce01fadd754b98339a2aba68889063e1249d7e4
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-19 01:38:10 -0500

    outsourced to other R file

--- R/03_actblue_fundraisers_match.R
+++ R/03_actblue_fundraisers_match.R
@@ -1,32 +1,4 @@
-source(here::here("R", "utilities.R"))
-
-# Load president, Senate, House candidate setup files ==========================
-source(here("R", "pcan_setup_2020.R"))
-source(here("R", "scan_setup_2020.R"))
-source(here("R", "hcan_setup_2020.R"))
-
-# Federal candidates (all), but just names =====================================
-cand_names <- list(
-  cand_pres_2020 %>% mutate(class = "pres"),
-  cand_senate_2020 %>% mutate(class = "us senate"),
-  cand_house_2020 %>% mutate(class = "us house")
-) %>%
-  map_dfr(
-    ~ .x %>%
-      select(first_name, last_name, class) %>%
-      mutate(name_full = paste(first_name, last_name)) %>%
-      dedup()
-  ) %>%
-  mutate(across(contains("name"), tolower)) %>%
-  bind_rows(
-    .,
-    {
-      .
-    } %>%
-      filter(name_full == "joseph biden") %>%
-      mutate(name_full = "joe biden")
-  )
-rm(cand_pres_2020, cand_senate_2020, cand_house_2020)
+source(here::here("R", "fec_cand_fundraiser_title.R"))
 
 # Read ActBlue entities, dedup. and match ======================================
 actblue_entities <- read_fst(
