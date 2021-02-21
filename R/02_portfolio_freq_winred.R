ee897914e36f61131fcc8ee12532634c6c4352e0
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-20 22:59:59 -0500

    Check for duplicate WinRed URLs as well

--- R/02_portfolio_freq_winred.R
+++ R/02_portfolio_freq_winred.R
@@ -2,17 +2,41 @@ categories <- "winred"
 source(here::here("R", "01_data_import.R"))
 
 # Wrote function: portfolio_summ ===============================================
-temp <- df_raw %>%
+df_raw <- df_raw %>%
   # To kick out duplicates
   mutate(url = gsub("\\?sc=winred-directory", "", url)) %>%
-  mutate(name = gsub(" for Congress", "", name)) %>%
+  mutate(name = gsub(" for Congress|For Congress|Friends of ", "", name)) %>%
   mutate(
-    race = ifelse(name == "Alison Hayden" & race == "CA-16", "CA-15", race),
-    race = ifelse(name == "Liz Marty May" & race == "SD-1", "SD-AL", race),
-    race = ifelse(name == "Matthew Morris" & race == "DE-NaN", "DE-AL", race),
-    race = ifelse(name == "Scott Perry" & race == "PA-4", "PA-10", race),
-    race = ifelse(name == "Vern Buchanan" & race == "FL-13", "FL-16", race)
+    race = case_when(
+      name == "Alison Hayden" & race == "CA-16" ~ "CA-15", 
+      name == "Liz Marty May" & race == "SD-1" ~ "SD-AL",
+      name == "Matthew Morris" & race == "DE-NaN" ~ "DE-AL",
+      name == "Scott Perry" & race == "PA-4" ~ "PA-10",
+      name == "Vern Buchanan" & race == "FL-13" ~ "FL-16",
+      TRUE ~ race
+    ),
+    race = gsub("-0", "-", race)
   ) %>%
+  mutate(
+    name = case_when(
+      name == "August Plugger" & grepl("august-pfluger-for-congress", url) ~
+        "August Pfluger",
+      name == "Ben Sasse for U.S. Senate, Inc." ~ "Ben Sasse",
+      name == "Cawthorn for NC" ~ "Madison Cawthorne",
+      name == "Elect Tuman" ~ "Doug Tuman",
+      name == "Garske" ~ "John Garske",
+      # GOP Strategist; seems a mistake
+      name == "Keats Norfleett" & grepl("gohmert", url) ~ "Louie Gohmert",
+      name == "MANGA Anantatmula" ~ "Manga Anantatmula",
+      name == "Marianette Miller-Meeks" ~ "Mariannette Miller-Meeks",
+      name == "Jon Huey" & grepl("mcclintock", url) ~ "Tom McClintock",
+      name == "Nick Lalota" ~ "Nick LaLota",
+      name == "steven raiser" ~ "Steven Raiser",
+      TRUE ~ name
+    )
+  )
+
+temp <- df_raw %>%
   # Changed jurisdiction but keep the record
   # filter(!(name == "Jimmy Rodriguez" & race == "AZ-8")) %>%
   filter(!is.na(url)) %>%
@@ -31,6 +55,13 @@ temp %>%
   table(useNA = "ifany")
 
 head(sort(table(temp$amount), decreasing = TRUE), 10)
+
+# In WinRed's case, there should be no within-URL changes ======================
+temp %>%
+  group_by(url, seq, amount) %>%
+  filter(n() > 1) %>%
+  nrow() %>%
+  {assert_that(. == 0)}
 
 # Top 5 Most Frequent Distributions ============================================
 p <- prop(temp, "amount", sort = TRUE, head = 5, print = FALSE) %>%
