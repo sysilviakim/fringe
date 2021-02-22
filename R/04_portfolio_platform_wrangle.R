ffad5208eacc5e42adda50b1ab0d6f21e4dd0449
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-21 23:28:27 -0500

    summ_calc_fxn to utilities

--- R/04_portfolio_platform_wrangle.R
+++ R/04_portfolio_platform_wrangle.R
@@ -1,29 +1,11 @@
 source(here::here("R", "utilities.R"))
 
-# Define function for various summary stats ====================================
-summ_fxn <- function(df) {
-  df %>%
-    rename(min_date = min, max_date = max) %>%
-    rowwise() %>%
-    mutate(
-      first = amount_split(amount) %>% .[1],
-      last = amount_split(amount) %>% .[length(.)],
-      min = amount_split(amount) %>% min(., na.rm = TRUE),
-      max = amount_split(amount) %>% max(., na.rm = TRUE),
-      mean = amount_split(amount) %>% mean(., na.rm = TRUE),
-      median = amount_split(amount) %>% median(., na.rm = TRUE),
-      q1 = amount_split(amount) %>% summary() %>% .[["1st Qu."]],
-      q3 = amount_split(amount) %>% summary() %>% .[["3rd Qu."]],
-      choices = amount_split(amount) %>% length()
-    )
-}
-
 # Import platform summarized data, create first/last/min/max ===================
 categories <- c("winred", "rightus", "actblue")
 dl <- categories %>%
   set_names(., .) %>%
   map(~ loadRData(here(paste0("data/tidy/portfolio_summ_", .x, ".Rda")))) %>%
-  map(summ_fxn)
+  map(summ_calc_fxn)
 
 # Augment "class" variables (WinRed/Right.us) ==================================
 # ActBlue's added in previous scripts
@@ -63,12 +45,13 @@ save(dl, file = here("data/tidy/portfolio_summ_platforms.Rda"))
 
 # Federal candidates ===========================================================
 dl <- loadRData(here("data/tidy/portfolio_summ_federal_first_only.Rda")) %>%
-  map(summ_fxn) %>%
+  map(summ_calc_fxn) %>%
   map(
     ~ .x %>%
       mutate(
         amount = case_when(
           amount == "0-1-2-3" ~ NA,
+          amount == "-999" ~ NA,
           TRUE ~ amount
         )
       )
