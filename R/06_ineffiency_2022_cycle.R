40015a4b46d0300b791eecaa17943f56ed58d2f5
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-24 21:03:01 -0500

    addtorow successfully implemented

--- R/06_ineffiency_2022_cycle.R
+++ R/06_ineffiency_2022_cycle.R
@@ -116,8 +116,8 @@ temp <- dl %>%
         orig_2800 = ifelse(sum(ineff_2800, na.rm = TRUE) > 0, TRUE, FALSE),
         adjust = ifelse(
           sum(ineff_2900, na.rm = TRUE) > 0 & sum(ineff_2800, na.rm = TRUE) > 0,
-          "Adjusted \\$2,800 to \\$2,900",
-          "Did Not Adjust \\$2,800 Maximum"
+          "Adjusted $2,800 to $2,900",
+          "Did Not Adjust $2,800 Maximum"
         ),
         winred = ifelse(any(grepl("winred", url)), "WinRed", "Other"),
         actblue = ifelse(any(grepl("actblue", url)), "ActBlue", "Other")
@@ -143,9 +143,9 @@ tab <- cross2(c("Dem", "Rep"), categories) %>%
   rename(" " = rownames) %>%
   xtable()
 
-highlight_xtable(
+print(
   tab,
-  file = here("tab", "max_adjust_congress_2022.tex"),
+  file = here("tab", "max_adjust_congress_by_party_2022.tex"),
   booktabs = TRUE, include.rownames = FALSE, floating = FALSE
 )
 
@@ -173,11 +173,41 @@ tab <- cross2(c("Dem", "Rep"), categories) %>%
   map(
     ~ prop(
       temp[[.x[[2]]]] %>% filter(orig_2800 == TRUE & party == .x[[1]]),
-      c("adjust", ifelse(.x[[1]] == "Rep", "winred", "actblue")), print = FALSE
+      c("adjust", ifelse(.x[[1]] == "Rep", "winred", "actblue")),
+      print = FALSE
     ) %>%
       rowid_matrix_to_df() %>%
       mutate(type = paste0(.x[[1]], " (", simple_cap(.x[[2]]), ")")) %>%
       select(rownames, type, everything())
-    )
+  )
 
+out <- bind_cols(
+  tab[[1]] %>% select(-type) %>% rename(" " = rownames),
+  tab[[2]] %>% select(-rownames, -type),
+  tab[[3]] %>% select(-rownames, -type),
+  tab[[4]] %>% select(-rownames, -type),
+  .name_repair = "minimal"
+) %>%
+  xtable(align = "llll|ll|ll|ll")
 
+addtorow <- list(
+  pos = list(-1),
+  command = paste0(
+    tab %>%
+      map_chr(
+        ~ paste0(
+          "& \\multicolumn{2}{c}{", .x$type[1], "}",
+          collapse = ""
+        )
+      ) %>%
+      paste0(collapse = ""),
+    "\\\\"
+  )
+)
+
+print(
+  out,
+  add.to.row = addtorow, hline.after = c(0),
+  file = here("tab", "max_adjust_congress_by_platform_2022.tex"),
+  booktabs = TRUE, include.rownames = FALSE, floating = FALSE
+)
