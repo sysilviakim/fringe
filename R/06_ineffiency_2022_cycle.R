058c008378e43f29605d22aa9594f902fe6cc79c
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-24 20:07:18 -0500

    greene + platform effect sanity check

--- R/06_ineffiency_2022_cycle.R
+++ R/06_ineffiency_2022_cycle.R
@@ -7,9 +7,13 @@ df_ls <- categories %>%
     ~ read_fst(here(paste0("data/tidy/", .x, "_2022.fst"))) %>%
       filter(!is.na(party)) %>%
       arrange(across(c(starts_with("state"), ends_with("name"), "date"))) %>%
-      filter(incumbent == TRUE) %>%
+      # Don't care about incumbents
+      filter(incumbent == TRUE & last_name != "Loeffler") %>%
       mutate(url = gsub(" .*?$|\\|.*?$", "", url)) %>%
-      filter(last_name != "Loeffler") %>%
+      # Multiple entity or irrelevant one time links
+      filter(
+        !grepl("8-races", url) & !grepl("official-trump-duncan-yard-signs", url)
+      ) %>%
       dedup()
   )
 
@@ -17,7 +21,7 @@ df_ls %>%
   map(~ assert_that(max(.x$date, na.rm = TRUE) > as.Date("2021-01-01")))
 
 # Note that Greene, LaTurner, and Mace's WinRed links were not scraped
-# due to a flaw in the scraper (when both Anedot and WinRed existed, 
+# due to a flaw in the scraper (when both Anedot and WinRed existed,
 # scraped only Anedot)
 
 # Create portfolio summaries ===================================================
@@ -27,23 +31,41 @@ dl <- df_ls %>%
       ex <- fed_exception_vars(.y)
       .x %>%
         ungroup() %>%
-        # Take out for now to feed into portfolio_summ
-        select(
-          -FEC_ID_cand, -party, -first_name, -incumbent, -contains("class")
-        ) %>%
         mutate(year = 2022, portfolio = as.numeric(portfolio)) %>%
         filter(!is.na(url) & url != "") %>%
         # Don't filter for !is.na(portfolio) just yet
-        group_by(across(c(setdiff(ex, "url"), "date"))) %>%
+        # group_by(across(c(setdiff(ex, "url"), "date"))) %>%
         # First URL recorded for a given date/amount
-        filter(url == first(url)) %>%
-        group_by(across(c(setdiff(ex, "url"), "date", "portfolio"))) %>%
+        # filter(url == first(url)) %>%
+        # group_by(across(c(setdiff(ex, "url"), "date", "portfolio"))) %>%
         # This enforces reordering from smallest number, however
-        slice(1) %>%
-        ungroup() %>%
+        # slice(1) %>%
+        # ungroup() %>%
         portfolio_summ(order_vars = ex)
     }
   )
+
+# Manual corrections ===========================================================
+dl$house <- dl$house %>%
+  mutate(
+    max = case_when(
+      last_name == "Greene" &
+        url == "https://secure.winred.com/marjorie-greene-for-congress/donate" &
+        amount == "25-50-100-250-500-1000-2800" ~
+      # https://web.archive.org/web/20210131090143/https://secure.winred.com/marjorie-greene-for-congress/donate
+      as.Date("2021-01-31"),
+      TRUE ~ max
+    ),
+    min = case_when(
+      last_name == "Greene" &
+        url == "https://secure.winred.com/marjorie-greene-for-congress/donate" &
+        amount == "25-50-100-250-500-1000-2900" ~
+      # https://web.archive.org/web/20210205002844/https://secure.winred.com/marjorie-greene-for-congress/donate
+      as.Date("2021-02-05"),
+      TRUE ~ min
+    )
+  )
+
 save(dl, file = here("data/tidy/portfolio_summ_federal_first_only_2022.Rda"))
 
 # Create summary statistics ====================================================
@@ -109,7 +131,8 @@ tab <- cross2(c("Dem", "Rep"), categories) %>%
   map(
     ~ prop(
       temp[[.x[[2]]]] %>% filter(orig_2800 == TRUE & party == .x[[1]]),
-      c("adjust"), print = FALSE
+      c("adjust"),
+      print = FALSE
     ) %>%
       rowid_matrix_to_df() %>%
       rename(
@@ -121,12 +144,31 @@ tab <- cross2(c("Dem", "Rep"), categories) %>%
   xtable()
 
 highlight_xtable(
-  tab, 
+  tab,
   file = here("tab", "max_adjust_congress_2022.tex"),
   booktabs = TRUE, include.rownames = FALSE, floating = FALSE
 )
 
 # Conditional probability by platform ==========================================
+# aka how much of this is a platform effect?
+temp %>%
+  imap(
+    ~ .x %>%
+      filter(
+        orig_2800 == TRUE & party == "Rep" &
+          winred == "WinRed" & grepl("Did", adjust)
+      )
+  )
+
+temp %>%
+  imap(
+    ~ .x %>%
+      filter(
+        orig_2800 == TRUE & party == "Dem" &
+          actblue == "ActBlue" & grepl("Did", adjust)
+      )
+  )
+
 tab <- cross2(c("Dem", "Rep"), categories) %>%
   map(
     ~ prop(
@@ -134,13 +176,8 @@ tab <- cross2(c("Dem", "Rep"), categories) %>%
       c("adjust", ifelse(.x[[1]] == "Rep", "winred", "actblue")), print = FALSE
     ) %>%
       rowid_matrix_to_df() %>%
-      mutate(type = paste0(.x[[1]], " (", simple_cap(.x[[2]]), ")"))
-  )
-
-highlight_xtable(
-  tab, 
-  file = here("tab", "max_adjust_congress_2022.tex"),
-  booktabs = TRUE, include.rownames = FALSE, floating = FALSE
-)
+      mutate(type = paste0(.x[[1]], " (", simple_cap(.x[[2]]), ")")) %>%
+      select(rownames, type, everything())
+    )
 
 
